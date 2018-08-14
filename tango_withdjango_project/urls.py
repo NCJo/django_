8fbf2360e8334b076e76ba2d50d371ec9a291061
NCJo <cnimkulr@gmail.com>
2018-08-13 19:11:17 -0600

    initialize repo

---
+++ tango_withdjango_project/urls.py
@@ -0,0 +1,32 @@
+"""tango_withdjango_project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from django.conf.urls import include
+from rango import views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index, name='index'),
+    url(r'^rango/', include('rango.urls')),
+        # above maps any urls starting with rango/
+        # to be handled by the rango application
+
+        # now need to create urls.py in rango app to handled
+        # remaining URL string (and map the empty string to
+        # the index view)
+]
+
