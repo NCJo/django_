4d1d2ed399ae5dd8d42662453e1e778c742fc66e
NCJo <cnimkulr@gmail.com>
2018-08-17 19:28:16 -0600

    Database is fully functional

---
+++ populate_rango.py
@@ -0,0 +1,67 @@
+# Make sure this file locate in the correct folder or it will throw import error
+
+import os
+# Very important or errors throw when try to import models
+os.environ['DJANGO_SETTINGS_MODULE'] = 'tango_withdjango_project.settings'
+
+import django
+django.setup()
+from rango.models import Category, Page
+
+def populate():
+    # First we create lists of dictionaries containing the pages we want to add into each category
+    # Then we will create a dictionary of dictionaries for our categories
+    # It allows us to iterate through each data structure and add the data to our models
+
+    # Add categories and pages here
+
+    python_pages = [
+        {"title": "Official Python Tutorial", "url": "http://doc.python.org/2/tutorial/"},
+        {"title":"How to think like a computer scientist", "url":"http://www.greenteapress.com/thinkpython/"},
+        {"title":"Learn Python in 10 Minutes", "url":"http://www.korokithakis.net/tutorials/python/"}
+    ]
+
+    django_pages = [
+        {"title":"Official Django Tutorial", "url":"http://docs.djangoproject.com/en/1.9/intro/tutorial01/"},
+        {"title":"Django Rocks", "url":"http://www.djangorocks.com/"},
+        {"title":"How to tango with django", "url":"http://tangowithdjango.com"}
+    ]
+
+    other_pages = [
+        {"title":"Bottle", "url":"http://bottlepy.org/docs/dev/"},
+        {"title":"Flask", "url":"http://flask.pocoo.org"},
+    ]
+
+    cats = {
+        "Python": {"pages": python_pages},
+        "Django": {"pages": django_pages},
+        "Other Frameworks": {"pages": other_pages}
+    }
+
+    # Go through cats dictionary -> add each category  -> add all the associated pages for that category
+    for cat, cat_data in cats.items():
+        c = add_cat(cat)
+        for p in cat_data["pages"]:
+            add_page(c, p["title"], p["url"])
+    
+    # Print out category added
+    for c in Category.objects.all():
+        for p in Page.objects.filter(category=c):
+            print("- {0} - {1}".format(str(c), str(p)))
+
+def add_page(cat, title, url, views=0):
+    p = Page.objects.get_or_create(category=cat, title=title)[0]
+    p.url=url
+    p.views=views
+    p.save()
+    return p
+
+def add_cat(name):
+    c = Category.objects.get_or_create(name=name)[0]
+    c.save()
+    return c
+
+# Start execution here
+if __name__ == '__main__':
+    print("Starting Rango population script...")
+    populate()
--- tango_withdjango_project/settings.py
+++ tango_withdjango_project/settings.py
@@ -14,9 +14,13 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print(BASE_DIR)
 
 # Path to templates directory
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'tango_withdjango_project/templates')
+print(TEMPLATE_DIR)
+
+# Path to static directory
 STATIC_DIR = os.path.join(BASE_DIR, 'tango_withdjango_project/static')
 print(STATIC_DIR)
 
